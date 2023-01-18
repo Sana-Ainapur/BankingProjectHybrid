36307c99d1d789cd41692d70dac79f8822df3442
Sana-Ainapur <sana.ainapur1@gmail.com>
2023-01-18 19:58:40 +0530

    my first commit

---
+++ src/main/java/com/bankingprojhybridfw/base/BaseClass.java
@@ -0,0 +1,15 @@
+package com.bankingprojhybridfw.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.guru99.com/V1/index.php");
+		
+	}
+
+}
